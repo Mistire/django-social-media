03f42f8cd39e3919b460793fe921a5c5dc31029c
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2025-04-04 12:27:41 -0400

    Fixed issues in User Registration feature

--- django_social_media/authentication/serializers.py
+++ django_social_media/authentication/serializers.py
@@ -1,20 +1,27 @@
 from .models import CustomUser
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
-
+    
     class Meta:
-        model = User
+        model = CustomUser
         fields = ["email", "username", "password"]
 
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
     def create(self, validated_data):
-        user = User.objects.create_user(
-            email=validated_data["email"],
-            username=validated_data["username"],
-            password=validated_data["password"],
-        )
+        password = validated_data.pop("password")
+        user = CustomUser.objects.create_user(password=password, **validated_data)
         return user
