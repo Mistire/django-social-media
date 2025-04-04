5b630e4a3f36b9e92e0a2b09d23e34e948db2bd0
Eyuel763 <eyuelg1997@gmail.com>
2025-04-04 17:47:46 +0400

    Delete the registration feature

--- django_social_media/authentication/views.py
+++ django_social_media/authentication/views.py
@@ -2,15 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer, LoginSerializer, TokenObtainPairSerializer
-
-class UserRegistrationView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from .serializers import LoginSerializer, TokenObtainPairSerializer
 
 class UserLoginView(APIView):
     def post(self, request):
