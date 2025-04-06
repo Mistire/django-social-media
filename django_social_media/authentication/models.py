00e9d0db02bf71f79e266510ffe391047651d919
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2025-04-06 12:43:58 +0300

    model for user profile

--- django_social_media/authentication/models.py
+++ django_social_media/authentication/models.py
@@ -2,31 +2,44 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 
 # Create your models here.
+
+
 class CustomUserManager(BaseUserManager):
-  def create_user(self, email, username, password=None, **extra_fields):
-    if not email:
-      raise ValueError("Email is required")
-    email = self.normalize_email(email)
-    user = self.model(email=email, username=username, **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
-  
-  def create_superuser(self, email, username, password=None, **extra_fields):
-    extra_fields.setdefault("is_staff", True)
-    extra_fields.setdefault('is_superuser', True)
-    return self.create_user(email, username, password, **extra_fields)
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, **extra_fields)
+
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-  email = models.EmailField(max_length=255, unique=True)
-  username = models.CharField(max_length=255, unique=True)
-  is_staff = models.BooleanField(default=False)
-  is_active = models.BooleanField(default=True)
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
-  objects = CustomUserManager()
+    objects = CustomUserManager()
 
-  USERNAME_FIELD = "email"
-  REQUIRED_FIELDS = ["username"]
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
-  def __str__(self):
-    return self.email
+    def __str__(self):
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def str(self):
+        return f"{self.user.username}'s Profile"
