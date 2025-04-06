51cd703aba9dbc6e8f7e2475a7313e6eb91c2088
Tewodros Anteneh <tewodrosanteneh@outlook.com>
2025-04-06 21:15:12 +0300

    Add update and delete functionality to PostViewSet

--- django_social_media/posts/views.py
+++ django_social_media/posts/views.py
@@ -1,9 +1,36 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from django.utils import timezone
 from .models import Post
 from .serializers import PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return Post.objects.all().order_by('-created_at')
+        return Post.objects.filter(user=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        post = self.get_object()
+        if post.user != self.request.user:
+            raise PermissionDenied("You don't have permission to edit this post.")
+        serializer.save(updated_at=timezone.now())
+
+    def perform_destroy(self, instance):
+        if instance.user != self.request.user:
+            raise PermissionDenied("You don't have permission to delete this post.")
+        instance.delete()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "Post deleted successfully"}, status=status.HTTP_200_OK)
