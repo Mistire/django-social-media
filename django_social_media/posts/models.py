4e49c11b70432308c88d5108cea5edfaa77cccde
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2025-04-09 14:48:45 +0300

    like model

--- django_social_media/posts/models.py
+++ django_social_media/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Post(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -13,3 +14,17 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post by {self.user.username} at {self.created_at}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['post', 'user']  # Ensure no duplicate likes
+
+    def __str__(self):
+        return f"{self.user.username} liked Post {self.post.id}"
