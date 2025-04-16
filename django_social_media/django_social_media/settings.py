70118cf900eb2dc5edf48b36bf32fa533632216b
Eyuel Gebreselassie <eyuelg1997@gmail.com>
2025-04-16 09:25:52 +0400

    Update settings.py

--- django_social_media/django_social_media/settings.py
+++ django_social_media/django_social_media/settings.py
@@ -107,11 +107,10 @@ WSGI_APPLICATION = 'django_social_media.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/django_social_media',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
