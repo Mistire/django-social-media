cf7f925d509d0bcbf38cca72bd364445c6a17906
Mistire <mdwithgod@gmail.com>
2025-05-03 11:01:02 +0300

    Fix delployment

--- django_social_media/django_social_media/settings.py
+++ django_social_media/django_social_media/settings.py
@@ -49,10 +49,6 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': [     
-    #     'rest_framework.permissions.AllowAny
-
-    # ], 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -109,7 +105,7 @@ WSGI_APPLICATION = 'django_social_media.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/django_social_media',
+        default='postgresql://django_social_mediadb_user:u0bkd4uOYk6e8lRoLrncNpHcPPBdahUu@dpg-cvtsvdngi27c73aaeaf0-a/django_social_mediadb',
         conn_max_age=600
     )
 }
